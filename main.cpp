3fcf420b27a27d9577eb369a6a2533367788004e
maina-0 <maina0@proton.me>
2025-03-31 19:01:35 +0300

    roy

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void key_callback(GLFWwindow *window,int key,int scancode,int action,int mode){
+    if(key==GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window,GL_TRUE);
+}
+
 int main(){ 
     glfwInit();
     glfwDefaultWindowHints();
-    if (glfwCreateWindow(1920,720,"OpenGL",nullptr,nullptr) == nullptr){
+    GLFWwindow* window = glfwCreateWindow(1920,720,"OpenGL",nullptr,nullptr);
+            if (window == nullptr){
         std::cout<<"failed to create GLFW window\n";
         glfwTerminate();
         return -1;
@@ -14,12 +20,27 @@ int main(){
 
     glfwMakeContextCurrent(window);
 
-    glewExperimental=GL_TRUE;
+ //   glewExperimental=GL_TRUE;
+
+    glfwSetKeyCallback(window,key_callback);
 
     if (glewInit()!= GLEW_OK){
         std::cout<<"Glew Failed to initialize\n";
         return -1;
     }
+
+    glViewport(0,0,1920,720);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+
+        glClearColor(0.9f,0.1f,0.9f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 
     return 0;
 }

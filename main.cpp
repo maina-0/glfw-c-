487a97de06697ac2774c73f27243154bfa2a282d
maina-0 <maina0@proton.me>
2025-04-08 19:01:29 +0300

    roy

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ void key_callback(GLFWwindow *window,int key,int scancode,int action,int mode){
         glfwSetWindowShouldClose(window,GL_TRUE);
 }
 
+GLfloat vertices[]={
+    -0.5f,-0.5f,0.0f,
+    0.5f,-0.5f,0.0f,
+    0.0f,0.5f,0.0f
+};
+
 int main(){ 
     glfwInit();
     glfwDefaultWindowHints();
@@ -29,12 +35,19 @@ int main(){
         return -1;
     }
 
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
     glViewport(0,0,1920,720);
 
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
 
-        glClearColor(0.9f,0.1f,0.9f,1.0f);
+        glClearColor(0.0f,0.5f,0.5f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glfwSwapBuffers(window);
